f5381d6958663a249e9d5cfa21d72f528ee83980
morganherlocker <morgan.herlocker@gmail.com>
2019-02-08 09:37:53 -0800

    add prettier and clean up formatting

--- wrap.js
+++ wrap.js
@@ -1,14 +1,16 @@
-const through2 = require('through2')
-const byline = require('byline')
+const through2 = require("through2");
+const byline = require("byline");
 
-console.log('{"type": "FeatureCollection","features": [')
+console.log('{"type": "FeatureCollection","features": [');
 
 process.stdin
-.pipe(byline.createStream())
-.pipe(through2((chunk, enc, next) => {
-  console.log(chunk.toString() + ',')
-  next()
-}))
-.on('finish', () => {
-  console.log(']}')
-})
+  .pipe(byline.createStream())
+  .pipe(
+    through2((chunk, enc, next) => {
+      console.log(chunk.toString() + ",");
+      next();
+    })
+  )
+  .on("finish", () => {
+    console.log("]}");
+  });
